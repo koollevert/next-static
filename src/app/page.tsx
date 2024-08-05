b6deef5c0ff15bf3b1e4148f46f1854c3c68c217
leVert <selmamathis15@gmail.com>
2024-08-05 18:26:26 +0300

    common ui with layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <div>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
-      </div>
+      
       <div>Home page</div>
     </div>
     
