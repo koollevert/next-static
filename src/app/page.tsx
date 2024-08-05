bd47d846f4b1beefcdb7a50cf642221b1caea34a
leVert <selmamathis15@gmail.com>
2024-08-05 18:52:04 +0300

    next images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image";
 import Link from 'next/link';
+import homeImg from 'public/home.jpg';
+
 
 export default function Home() {
   return (
     <div>
-      
-      <div>Home page</div>
+      Home page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="two factory mates and a car"
+          fill
+          style={{objectFit: 'cover'}}
+        />
+      </div>
     </div>
     
   );
